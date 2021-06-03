6d2285bc9de38fcc5e284a7766b56c2006bbb193
qtKite <kite@uwu.icu>
2021-06-03 13:42:49 +1000

    dumper files

---
+++ src/dumper/dllmain.cpp
@@ -0,0 +1,28 @@
+// this is to poc for dumping out registry files as part 2 of the reversal
+//
+// TO-DO:
+// import detours
+// write hook functions
+// inject and write findings
+// list of functions to hook:
+// 
+
+
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
