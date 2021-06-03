f9ccb5be9fac82a0bacc60c11f3053cdba8f020f
qtKite <kite@uwu.icu>
2021-06-03 16:14:25 +1000

    to-do for dcontrol

--- src/defender-control/main.cpp
+++ src/defender-control/main.cpp
@@ -3,11 +3,18 @@
 // We are going to reverse engineer the d-control from sordum
 // and build an open source safe version since i struggle trust
 // defender control cause of the virus total false positivies
+// to-do:
+// finish dumper
+// write poc
+// write argument parser
+// create cli program
+// maybe make a ui for this
 
 // entrypoint
 //
 int main()
 {
+
 
   return 0;
 }
