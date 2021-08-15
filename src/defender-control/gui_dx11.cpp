a2a1913316fd815e9cfb096cfebd6e0dc9ffa1e5
qtkite <kite@uwu.icu>
2021-08-15 15:35:45 +1000

    dx11 template functions

--- src/defender-control/gui_dx11.cpp
+++ src/defender-control/gui_dx11.cpp
@@ -2,5 +2,30 @@
 
 namespace gui::dx11
 {
+  bool create_device(HWND hwnd)
+  {
+    // Setup a swapchain
 
+    return false;
+  }
+
+  void cleanup()
+  {
+  }
+
+  void setup(HWND hwnd)
+  {
+  }
+
+  void start()
+  {
+  }
+
+  void end()
+  {
+  }
+
+  void resize()
+  {
+  }
 }
--- src/defender-control/gui_dx11.hpp
+++ src/defender-control/gui_dx11.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <Windows.h>
-
+#include <d3d11.h>
+#include <tchar.h>
 #include <imgui.h>
 
 namespace gui::dx11
 {
+  bool create_device(HWND hwnd);
+  void cleanup();
+  void setup(HWND hwnd);
+  void start();
+  void end();
 
+  // resize here:
+
+  void resize();
 }
