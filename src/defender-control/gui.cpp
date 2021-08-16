56f9d8a6f3794666e75275a48314ff0e59d79b9c
qtkite <kite@uwu.icu>
2021-08-16 21:41:26 +1000

    wndproc

--- src/defender-control/gui.cpp
+++ src/defender-control/gui.cpp
@@ -2,8 +2,42 @@
 
 namespace gui
 {
+  LRESULT CALLBACK window_proc(const HWND hwnd, const UINT msg,
+    const WPARAM wParam, const LPARAM lParam)
+  {
+    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam))
+      return true;
+
+    // TODO:
+    switch (msg)
+    {
+    case WM_SIZE:
+      return 0;
+    case WM_SYSCOMMAND:
+      return 0;
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+    }
+
+    return DefWindowProcA(hwnd, msg, wParam, lParam);
+  }
+
   int main()
   {
+    WNDCLASSEXW wc{
+      sizeof(WNDCLASSEXW), CS_CLASSDC, window_proc, 0L, 0L,
+      GetModuleHandle(0), NULL, NULL, NULL, NULL, L"dx", NULL
+    };
+
+    RegisterClassExW(&wc);
+
+    auto hwnd = CreateWindowExW(
+      0, wc.lpszClassName, L"dx", WS_OVERLAPPEDWINDOW, 0, 0, 1920, 1080, 0, 0, wc.hInstance, 0
+    );
+
+    // TODO:
+    //
 
     return EXIT_SUCCESS;
   }
--- src/defender-control/gui.hpp
+++ src/defender-control/gui.hpp
@@ -1,5 +1,9 @@
 #pragma once
 #include <Windows.h>
+#include <imgui_impl_win32.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+  HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 namespace gui
 {
